8e52ea1f10929a32491fe076700baa17b640f260
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-21 22:23:04 +0300

    feat: expose local project APIs

--- packages/projects/src/index.ts
+++ packages/projects/src/index.ts
@@ -1,1 +1,48 @@
-export {};
+export {
+  DEFAULT_PROJECT_UI,
+  DEFAULT_PROJECT_VIEWPORT,
+  MAX_PROJECT_NAME_LENGTH,
+  MAX_PROJECT_SCALE,
+  MIN_PROJECT_SCALE,
+  PROJECT_STORAGE_VERSION,
+  ProjectValidationError,
+  createProject,
+  duplicateProject,
+  normalizeProjectName,
+  parseAndMigrateDocument,
+  parseDocumentInput,
+  renameProject,
+  replaceProjectDocument,
+  replaceProjectUi,
+  replaceProjectViewport,
+  validateProject,
+  validateProjectUi,
+  validateProjectViewport,
+} from "./project";
+export type {
+  CreateProjectInput,
+  ProjectUiState,
+  ProjectViewport,
+  VlezetProjectRecord,
+} from "./project";
+
+export { MemoryProjectRepository } from "./repository";
+export type { ProjectRepository } from "./repository";
+
+export {
+  IndexedDbProjectRepository,
+  ProjectStorageError,
+  createIndexedDbProjectRepository,
+} from "./indexeddb";
+
+export {
+  ProjectFileError,
+  parseProjectFile,
+  projectFileSlug,
+  projectJsonFilename,
+  serializeProjectFile,
+} from "./file-format";
+export type { ParseProjectFileOptions, ProjectFileErrorCode } from "./file-format";
+
+export { AutosaveCoordinator } from "./autosave";
+export type { AutosaveCoordinatorOptions, SaveStatus } from "./autosave";
