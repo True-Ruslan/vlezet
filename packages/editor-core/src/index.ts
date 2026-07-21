d7e569c692503f7041d4684df1e71882b7fd2ba7
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-21 19:12:11 +0300

    feat(editor-core): export topology editing APIs

--- packages/editor-core/src/index.ts
+++ packages/editor-core/src/index.ts
@@ -1,4 +1,19 @@
-export type { EditorCommand } from "./commands";
+export type { EditorCommand, EditorCommandLabel } from "./commands";
 export { createHistoryState, executeCommand, redo, undo } from "./history";
 export type { HistoryEntry, HistoryState } from "./history";
-export { setWallLength, wallLength } from "./wall-editing";
+export {
+  addConnectedWall,
+  addTJunctionWall,
+  addTopologicalWall,
+  MAX_WALL_THICKNESS_MM,
+  MIN_WALL_THICKNESS_MM,
+  moveVertex,
+  setTopologicalWallLength,
+  setWallThickness,
+  topologicalWallLength,
+} from "./topology-editing";
+export type {
+  AddTopologicalWallInput,
+  DocumentEdit,
+  WallEndpointIntent,
+} from "./topology-editing";
