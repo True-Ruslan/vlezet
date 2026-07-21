d18df04995eed0884decf03a99c132e618199b44
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-21 19:09:25 +0300

    feat(geometry): export topology APIs

--- packages/geometry/src/index.ts
+++ packages/geometry/src/index.ts
@@ -1,7 +1,24 @@
+export { validateTopology } from "./diagnostics";
+export type { TopologyDiagnostic, TopologyDiagnosticCode } from "./diagnostics";
 export { chooseGridStep } from "./grid";
 export { distanceBetween } from "./point";
 export type { Point2 } from "./point";
+export {
+  GEOMETRY_EPSILON_MM,
+  isProperInteriorIntersection,
+  pointOnSegment,
+  projectPointToSegment,
+  segmentIntersection,
+} from "./segment";
+export type { SegmentIntersection, SegmentProjection } from "./segment";
 export { snapWallPoint } from "./snapping";
 export type { SnapGuide, SnapKind, SnapResult, SnapWallPointInput } from "./snapping";
+export { deriveAtomicWallEdges, topologyVertexMap, wallRunLength } from "./topology";
+export type {
+  AtomicWallEdge,
+  TopologyDocumentLike,
+  TopologyVertexLike,
+  TopologyWallLike,
+} from "./topology";
 export { screenToWorld, worldToScreen, zoomViewportAt } from "./viewport";
 export type { ViewportTransform, ZoomLimits } from "./viewport";
