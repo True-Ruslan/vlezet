ea4bac7c232aae1d9ac71592cab83e802923b7ef
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-22 19:29:32 +0300

    feat: export spatial object projection contract

--- packages/spatial/src/index.ts
+++ packages/spatial/src/index.ts
@@ -1,7 +1,12 @@
-export { DEFAULT_WALL_HEIGHT_MM, projectDocumentToSpatialScene } from "./project-document";
+export {
+  DEFAULT_OBJECT_HEIGHT_MM,
+  DEFAULT_WALL_HEIGHT_MM,
+  projectDocumentToSpatialScene,
+} from "./project-document";
 export type {
   Point3,
   SpatialFloor,
+  SpatialObject,
   SpatialOpeningMarker,
   SpatialProjectionDiagnostic,
   SpatialProjectionResult,
