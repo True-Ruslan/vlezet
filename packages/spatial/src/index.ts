75d781a028c27c96454e88b62e9a1677d9c2b8dc
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-22 18:13:50 +0300

    feat: export spatial projection API

---
+++ packages/spatial/src/index.ts
@@ -0,0 +1,10 @@
+export { DEFAULT_WALL_HEIGHT_MM, projectDocumentToSpatialScene } from "./project-document";
+export type {
+  Point3,
+  SpatialFloor,
+  SpatialOpeningMarker,
+  SpatialProjectionDiagnostic,
+  SpatialProjectionResult,
+  SpatialScene,
+  SpatialWallSegment,
+} from "./types";
