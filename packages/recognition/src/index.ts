162451239c9aef6241dad02f60e286f85d09490b
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-02 19:19:17 +0300

    refactor: make engine version a single public source

--- packages/recognition/src/index.ts
+++ packages/recognition/src/index.ts
@@ -1,6 +1,19 @@
 export * from "./architectural-lines";
 export * from "./cloud-sanity";
-export * from "./local-lines";
+export { LOCAL_RECOGNITION_ENGINE_VERSION } from "./engine-version";
+export {
+  analyzeWallCandidates,
+  buildWallCandidates,
+  createAdaptiveLocalRecognitionOptions,
+  DEFAULT_LOCAL_RECOGNITION_OPTIONS,
+} from "./local-lines";
+export type {
+  AdaptiveLocalRecognitionScaleInput,
+  BuildWallCandidatesInput,
+  DetectedLineSegment,
+  LocalRecognitionOptions,
+  LocalWallCandidateAnalysis,
+} from "./local-lines";
 export * from "./model";
 export * from "./openings";
 export * from "./provider";
